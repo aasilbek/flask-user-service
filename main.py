7f791522fa456303ea9483ab39dd1e33290c4667
Asilbek <asilbek.aliev.1999@mail.ru>
2021-11-15 10:14:09 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from app import cli, create_app, db
+from app.models import Message, Notification, Post, Task, User
+
+app = create_app()
+cli.register(app)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"db": db, "User": User, "Post": Post, "Message": Message,
+            "Notification": Notification, "Task":Task}
